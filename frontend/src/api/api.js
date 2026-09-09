61aaa109b8facdc0370f5aa51a40240f70b01aac
MurunwaMudzhadzhi <m.murunwa777@gmail.com>
2026-09-09 09:56:18 +0200

    Point frontend at deployed Render backend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const client = axios.create({
-  baseURL: 'http://localhost:8080/api',
+    baseURL: 'https://enviro365-withdrawal-system.onrender.com/api',
 })
 
 export const getInvestors = () => client.get('/investors').then(r => r.data)
